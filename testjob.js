1560c74948574a4c3a9ffcac4b570dcc43e8d6ca
Zolion <tom_soler@outlook.com>
2020-05-16 11:38:51 -0400

    ok

---
+++ testjob.js
@@ -0,0 +1,8 @@
+function parseToRoman(number) {
+    
+}
+
+parseToRoman(4); // === "IV"
+parseToRoman(37); // === "XXXVII"
+parseToRoman(143); // === "CXLIII"
+parseToRoman(1234); // === "MCCXXXIV"
